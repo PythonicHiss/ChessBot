11d352d58ccda120ffaf4638dc77c8887f0b291b
PythonicHiss <135261945+PythonicHiss@users.noreply.github.com>
2024-08-08 16:08:20 +0100

    Update Pieces.py

--- ChessBot/Pieces.py
+++ ChessBot/Pieces.py
@@ -1,54 +1,52 @@
+"""WIP, code currently bugged"""
+
 from enum import IntEnum
 import numpy as np
 
 
 class Piece(IntEnum):
         #Pieces stored as binary with colour + value  col|value -> 00|000
-    PAWN = 1
-    KNIGHT = 2
-    BISHOP = 3 
-    ROOK = 4
-    QUEEN = 5
-    KING = 6 
+    P = 1
+    N = 2
+    B = 3 
+    R = 4
+    Q = 5
+    K = 6 
 
-    WHITE = 16
-    BLACK = 8
+    w = 16
+    b = 8
+
 
 
 class ChessBoard():
     def __init__(self):
-        self.pieces = {
-            "KINGWHITE": np.uint64(0b0000000000000000000000000000000000000000000000000000000000010000),
-            "QUEENWHITE": np.uint64(0b0000000000000000000000000000000000000000000000000000000000001000),
-            "BISHOPWHITE": np.uint64(0b0000000000000000000000000000000000000000000000000000000000100100),
-            "KNIGHTWHITE": np.uint64(0b0000000000000000000000000000000000000000000000000000000001000010),
-            "ROOKWHITE": np.uint64(0b0000000000000000000000000000000000000000000000000000000010000001),
-            "PAWNWHITE": np.uint64(0b0000000000000000000000000000000000000000000000001111111100000000),
-            "PAWNBLACK": np.uint64(0b0000000011111111000000000000000000000000000000000000000000000000),
-            "ROOKBLACK": np.uint64(0b1000000100000000000000000000000000000000000000000000000000000000),
-            "KNIGHTBLACK": np.uint64(0b0100001000000000000000000000000000000000000000000000000000000000),
-            "BISHOPBLACK": np.uint64(0b0010010000000000000000000000000000000000000000000000000000000000),
-            "QUEENBLACK": np.uint64(0b0000100000000000000000000000000000000000000000000000000000000000),
-            "KINGBLACK": np.uint64(0b0001000000000000000000000000000000000000000000000000000000000000)
-            }
-        self.BoardReset() 
+        self.BoardReset()
 
     def BoardReset(self):
-        bitboard = np.uint64(0)
-        for piece in self.pieces.values():
-            bitboard |= piece
+        self.loc = {
+            "wK": np.uint64(0b0000000000000000000000000000000000000000000000000000000000010000),
+            "wQ": np.uint64(0b0000000000000000000000000000000000000000000000000000000000001000),
+            "wB": np.uint64(0b0000000000000000000000000000000000000000000000000000000000100100),
+            "wN": np.uint64(0b0000000000000000000000000000000000000000000000000000000001000010),
+            "wR": np.uint64(0b0000000000000000000000000000000000000000000000000000000010000001),
+            "wP": np.uint64(0b0000000000000000000000000000000000000000000000001111111100000000),
+            "bP": np.uint64(0b0000000011111111000000000000000000000000000000000000000000000000),
+            "bR": np.uint64(0b1000000100000000000000000000000000000000000000000000000000000000),
+            "bN": np.uint64(0b0100001000000000000000000000000000000000000000000000000000000000),
+            "bB": np.uint64(0b0010010000000000000000000000000000000000000000000000000000000000),
+            "bQ": np.uint64(0b0000100000000000000000000000000000000000000000000000000000000000),
+            "bK": np.uint64(0b0001000000000000000000000000000000000000000000000000000000000000)
+            }
     
     def PieceTypePos(self, piece, color):
-        return self.pieces.get((f'{piece.name+color.name}'), np.uint64(0))
-
-
-
-##TESTING##
-# chess_board = ChessBoard()
-# king_white_pos = chess_board.PieceTypePos(Piece.KING, Piece.WHITE)
-
-# print('{:064b}'.format(king_white_pos))
-
-
+        return self.loc.get((f'{color}{piece}'), np.uint64(0))
+    
+    def Update(self, piece, color, newPosBitboard):
+        key = f'{color}{piece}'
+        if key in self.loc: 
+            self.loc[key] =  np.uint64(self.loc[key]) ^  (np.uint64(self.loc[key]) | np.uint64(newPosBitboard)) 
+        else: 
+            raise ValueError(f"Invalid piece/color")
+        
 
 
